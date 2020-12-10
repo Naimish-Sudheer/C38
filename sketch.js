5bd8974be4ce3ed261f1791f7ce5735e7b9c11ac
Naimish-Sudheer <66679435+Naimish-Sudheer@users.noreply.github.com>
2020-12-10 16:03:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+var database;
+var form,player,game;
+var playerCount,gameState=0;
+var allPlayers;
+var car1,car2,car3,car4,cars,car1Image,car2Image,car3Image,car4;
+var track,ground
+
+function preload (){
+    car1Image=loadImage("images/car1.png")
+    car2Image=loadImage("images/car2.png")
+    car3Image=loadImage("images/car3.png")
+    car4Image=loadImage("images/car4.png")
+    ground=loadImage("images/ground.png")
+    track=loadImage("images/track.jpg")
+}
+
+
+function setup(){
+    createCanvas(displayWidth,displayHeight);
+    database=firebase.database();
+    game=new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+if (playerCount===4){
+    game.update(1)
+}
+if (gameState===1){
+    clear()
+    game.play()
+
+}
+
+}
